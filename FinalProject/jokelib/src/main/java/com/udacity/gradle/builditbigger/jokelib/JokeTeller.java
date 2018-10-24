e0f9f281b497d9a340eb8e1056c8dbe65e3ab497
Eric Braga <ericbraga25@gmail.com>
2018-10-24 10:32:19 -0300

    #1: Create a Java Library to tell some jokes

---
+++ FinalProject/jokelib/src/main/java/com/udacity/gradle/builditbigger/jokelib/JokeTeller.java
@@ -0,0 +1,9 @@
+package com.udacity.gradle.builditbigger.jokelib;
+
+public class JokeTeller {
+
+    public String tellMeAJoke() {
+        return "What is the biggest lie in the entire universe? 'I have read and agree to the Terms & Conditions.'";
+    }
+
+}
